1c66fc4a7b581189ab327a3ead10d11e71c57716
Indre Bakeer <indre.bakeer@gmail.com>
2019-02-14 03:30:13 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,8 @@
 require 'pry'
 
 class Artist
-  extend Memorable, Findable
-  include Paramable
+  extend Findable, Memorable::ClassMethods
+  include Paramable, Memorable::InstanceMethods
 
   attr_accessor :name
   attr_reader :songs
